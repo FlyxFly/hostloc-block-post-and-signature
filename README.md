9466716b9917437499a736808c6074bc38ca7243
Felix <kiwi1220@gmail.com>
2022-07-09 08:28:56 +0800

    重构

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # hostloc-block-post-and-signature
-屏蔽指定用户的发帖、回帖、签名
+用于屏蔽指定用户的发帖、回帖、签名
+
+# 安装
+- 先安装Tampermonkey
+- 导入此脚本即可
+
+# 编辑列表
+需要编辑脚本头部的代码
--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // @version      0.2
 // @description  根据关键字和用户名屏蔽帖子
 // @author       kiwi
+// @homepage     https://github.com/the1812/Bilibili-Evolved
 // @match        https://hostloc.com/forum-*
 // @match        https://hostloc.com/thread-*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -12,82 +13,133 @@
 
 (function() {
     'use strict';
+
+    class HostLocBlocker{
+        __construct(){
+
+            this.config={
+                blockedUser:[],
+                blockedKeyword:[],
+                blockedSignatureUser:[]
+            }
+            this.contentStorage=[];
+
+        }
+
+        save(){
+
+        }
+
+        restore(){
+            const data=localStorage.getItem('HostLocBlocker');
+            if(data){
+                const jsonData=JSON.parse(data);
+                for (x in ['blockedUser','blockedKeyword','blockedSignatureUser']) {
+                    this[x]=jsonData[x];
+                }
+            } 
+        }
+
+        hideFromList(){
+            document.querySelectorAll('#threadlisttableid tbody').forEach((item,index)=>{
+                if(item.id.includes('normalthread')){
+                    const title=item.querySelector('a.s.xst').innerText;
+                    for (let i = blockedKeyword.length - 1; i >= 0; i--) {
+                        if(title.includes(blockedKeyword[i])){
+                            // item.querySelector('a.s.xst').innerText='已屏蔽';
+                            item.style.display='none';
+                            break;
+                        }
+                    }
+
+
+                    const nameA=item.querySelectorAll('td.by')[0].querySelector('a');
+                    if(nameA){
+                        const userName=nameA.innerText.trim();
+                        if(blockedUser.includes(userName)){
+                            // item.querySelector('a.s.xst').innerText='已屏蔽';
+                            item.style.display='none';
+                        }
+                    }
+                }
+            })
+
+        }
+
+        hideReplyAndSignature(){
+            document.querySelectorAll('#postlist>div').forEach((item)=>{
+                if(!item.id.includes('post_')){
+                    return false;
+                }
+                const userLink=item.querySelector('a.xw1');
+                if(userLink){
+                    const userName=userLink.innerText.trim();
+                    if(userName && blockedUser.includes(userName)){
+                        item.style.display='none';
+                    }else if(blockedSignatureUser.includes(userName)){
+                        const signature=item.querySelector('div.sign');
+                        const contentText=signature.innerText;
+                        const contentHTML=signature.innerHTML;
+                        const storageKey=item.id+'signature';
+                        contentStorage[storageKey]=contentHTML;
+                        signature.innerHTML=`<span style="font-style:italic;font-size:10px;color:gray" class="hidden-by-script" data-restore-key="${storageKey}" title="${contentText}">已屏蔽,鼠标移到此处查看内容,点击还原内容</span>`;
+                    }
+                }
+
+                const tds=item.querySelectorAll('td');
+                tds.forEach((td)=>{
+                    if(td.id.includes('postmessage_')){
+                        const content=td.innerText;
+                        for (let i = blockedKeyword.length - 1; i >= 0; i--) {
+                            if(content.includes(blockedKeyword[i])){
+                                const contentHTML=td.innerHTML;
+                                const contentText=td.innerText;
+                                contentStorage[item.id]=contentHTML;
+                                td.innerHTML=`<span style="font-style:italic;font-size:10px;color:gray" class="hidden-by-script" data-restore-key="${item.id}" title="${content}">已屏蔽，鼠标移到此处查看内容</span>`;
+                                break;
+                            }
+
+                        }
+                    }
+                })
+            })
+        }
+
+        hideSignature(){
+
+        }
+
+
+        init(){
+            this.restore();
+            
+            if(location.href.includes('forum')){
+                this.hideFromList();
+            }
+
+            if(location.href.includes('thread')){
+                this.hideReply();
+            }
+
+            //监听点击事件，恢复被屏蔽的签名和帖子
+            document.querySelector('#postlist').addEventListener('click',(e)=>{
+                const item=e.target;
+                if(item.className.includes('hidden-by-script')){
+                    item.innerHTML=this.contentStorage[item.dataset.restoreKey]
+                }
+            })
+        }
+
+
+    }
+
+
+    const app=new HostLocBlocker();
+    app.init();
     const blockedUser=['CMBCHINA','等1下™我老公呢','朕的大清完了？','optimism','朕射你无罪','svd1983','Erik'];
     const blockedKeyword=['良心云','腾讯云','轻量','清凉','loudcone','绿云','webhorizon','pacificrack','斯巴达'];
     const blockedSignatureUser=['louiejordan','燕十三丶','快上车'];
     const contentStorage=[];//用于临时存储被屏蔽的帖子和签名内容
-
-    // 帖子列表页
-    if(location.href.includes('forum')){
-        document.querySelectorAll('#threadlisttableid tbody').forEach((item,index)=>{
-            if(item.id.includes('normalthread')){
-                const title=item.querySelector('a.s.xst').innerText;
-                for (let i = blockedKeyword.length - 1; i >= 0; i--) {
-                    if(title.includes(blockedKeyword[i])){
-                        // item.querySelector('a.s.xst').innerText='已屏蔽';
-                        item.style.display='none';
-                        break;
-                    }
-                }
-
-
-                const nameA=item.querySelectorAll('td.by')[0].querySelector('a');
-                if(nameA){
-                    const userName=nameA.innerText.trim();
-                    if(blockedUser.includes(userName)){
-                        // item.querySelector('a.s.xst').innerText='已屏蔽';
-                        item.style.display='none';
-                    }
-                }
-            }
-        })
-    }
-
-    // 帖子内容页
-    if(location.href.includes('thread')){
-        //监听点击事件，恢复被屏蔽的签名和帖子
-        document.querySelector('#postlist').addEventListener('click',(e)=>{
-            const item=e.target;
-            if(item.className.includes('hidden-by-script')){
-                item.innerHTML=contentStorage[item.dataset.restoreKey]
-            }
-        })
-        //遍历发帖和回复
-        document.querySelectorAll('#postlist>div').forEach((item)=>{
-            if(!item.id.includes('post_')){
-                return false;
-            }
-            const userLink=item.querySelector('a.xw1');
-            if(userLink){
-                const userName=userLink.innerText.trim();
-                if(userName && blockedUser.includes(userName)){
-                    item.style.display='none';
-                }else if(blockedSignatureUser.includes(userName)){
-                    const signature=item.querySelector('div.sign');
-                    const contentText=signature.innerText;
-                    const contentHTML=signature.innerHTML;
-                    const storageKey=item.id+'signature';
-                    contentStorage[storageKey]=contentHTML;
-                    signature.innerHTML=`<span style="font-style:italic;font-size:10px;color:gray" class="hidden-by-script" data-restore-key="${storageKey}" title="${contentText}">已屏蔽,鼠标移到此处查看内容,点击还原内容</span>`;
-                }
-            }
-
-            const tds=item.querySelectorAll('td');
-            tds.forEach((td)=>{
-                if(td.id.includes('postmessage_')){
-                    const content=td.innerText;
-                    for (let i = blockedKeyword.length - 1; i >= 0; i--) {
-                        if(content.includes(blockedKeyword[i])){
-                            const contentHTML=td.innerHTML;
-                            const contentText=td.innerText;
-                            contentStorage[item.id]=contentHTML;
-                            td.innerHTML=`<span style="font-style:italic;font-size:10px;color:gray" class="hidden-by-script" data-restore-key="${item.id}" title="${content}">已屏蔽，鼠标移到此处查看内容</span>`;
-                            break;
-                        }
-
-                    }
-                }
-            })
-        })
+ 
     }
 })();
